8b5fb55a6bfe0d1075c7a9231f87d4dc569033c8
Ragashree <ragashreeshekar@gmail.com>
2023-03-24 01:07:55 -0400

    Update gradecheck-02.sh

--- gradecheck-02.sh
+++ gradecheck-02.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 hostname="fureveranimalrescue.aia.local"
 port="443"
-http_response=$(curl --write-out %{http_code} --silent --output /dev/null https://$hostname:$port)
+http_response=$(curl -k --write-out %{http_code} --silent --output /dev/null https://$hostname:$port)
 
 if [ $http_response -eq 200 ]; then
     echo "Gradecheck-2 Success!"
